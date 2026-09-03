add754977abf3094931ba7d337b53dd2643fdc1c
Lídia Carvalho <lidiapcarvalho12@gmail.com>
2026-09-03 18:34:21 +0100

    [03/09/2026] Data exploration with Pandas and Plotly

--- notes.md
+++ notes.md
@@ -4,6 +4,8 @@
 
 ## [`src/main.py`](src/main.py)
 
+DataFrame - estrutura de dados do pandas que organiza informação em forma de tabela, composta por linhas e colunas. No projeot, o DataFrame `books` representa a tabela com os livros lidos, onde cada linhas corresponde a um livro e cada coluna a uma caracterísitca do livro.
+
 `as pd` - abreviatura convencional para pandas
 
 `Index()` - estrutura de pandas para representar uma sequência de nomes
@@ -23,3 +25,40 @@
 
 Repitimos duas vezes os parênteses retos ("[]"), pois o segundo é uma lista de nomes.
 Ao escolher apenas uma coluna, não é necessário repetir os parênteses retos, pois estamos apenas a fornecer uma string.
+
+`books['year_read'] == 2022` - cria a condição True or False
+
+`books[ ... ]` - usa essa condição para selecionar as linhas correspondentes
+
+Juntando:
+`books[books['year_read'] == 2022]` - filtra o DataFrame
+
+**Operadores**
+==    igual a
+!=    diferente de
+>     maior que
+<     menor que
+>=    maior ou igual a
+<=    menor ou igual a
+
+`.value_counts()` - conta quantas vezes cada valor aparece, e por defeito, ordena os resultados do maior para o menor número de ocorrências
+
+**Usando o Plotly**
+```python
+fig = px.bar(
+    country_counts,
+    x=country_counts.index,
+    y=country_counts.values,
+    text=country_counts.values
+)
+
+fig.show()
+```
+`fig` - variável onde guardamos o objeto gráfico criado pelo Plotly
+`px.bar(...)` - cria o gráfico
+`fig.show()` - mostra o gráfico
+
+
+
+
+
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 # as  pd - abreviatura convencional para pandas
+import plotly.express as px
 
 books = pd.read_csv('data/books.csv')
 # lê o ficheiro CSV e armazena os dados no DataFrame do pandas
@@ -26,3 +27,47 @@ print(books['title'])  # Mostra a coluna 'title'
 
 # Mais de uma coluna específica
 print(books[['title', 'author']])  # Mostra as colunas 'title' e 'author'
+
+# Quais os livros que li em 2022?
+# Condição para filtrar os livros lidos em 2022
+# Aqui para cada linha, ele vai responder True or False
+print(books['year_read'] == 2022)
+
+# Aqui ele vai mostrar apenas os livros lidos em 2022
+print(books[books['year_read'] == 2022])
+
+print(books['year_read'] == 2025)
+print(books[books['year_read'] == 2025])
+
+# Filtrar pelo rating
+print(books[books['rating'] == 5])  # Mostra os livros com rating igual a 5
+# Mostra os livros com rating maior ou igual a 4
+print(books[books['rating'] >= 4.5])
+
+# Mais de uma condição
+print(books[(books["year_read"] == 2025) & (books["rating"] == 5)])
+
+print(books[(books["year_read"] == 2022) | (books["year_read"] == 2025)])
+
+# Quantos livros temos de cada país?
+country_counts = books['country'].value_counts()
+print(country_counts)
+
+# Plotly
+fig = px.bar(
+    country_counts,
+    x=country_counts.index,  # x - países
+    y=country_counts.values,  # y - número de livros
+    text=country_counts.values  # texto a mostrar em cada barra
+)
+
+fig.update_layout(
+    xaxis_title="País",
+    yaxis_title="Número de livros",
+)
+
+fig.update_traces(
+    textposition='outside'  # Posição do texto fora da barra
+)
+
+fig.show()
